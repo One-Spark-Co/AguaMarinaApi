b692aba15cb5116518c4b5fa88920e555f5e63bc
Jorge Nava de la Peña <jorge.pena@ibm.com>
2023-04-04 11:26:22 -0600

    Feat :sparkles:[SetUserLiters]: Finished serverless functions

--- agua-marina-functions/packages/functions/setUserLiters/setUserLiters.js
+++ agua-marina-functions/packages/functions/setUserLiters/setUserLiters.js
@@ -2,25 +2,14 @@
 const axios = require('axios');
 
 async function main(args) {
-  /*
-    Get order id
-    Get order
-    Get user from order
-    Get product bought liters from order
-    Sum actual user liters with the ones bought
-    Update user information
-  */
-  console.log('[NAVA] args :', args);
-  console.log('[NAVA] process.env :', process.env.API_URL);
+  console.log('> input :', args)
   const API_URL = process.env.API_URL;
   const AUTH_TOKEN = process.env.AUTH_TOKEN;
   const USER_AGENT = process.env.USER_AGENT;
   const ORDER_ID = args.orderId;
+  const LITERS_PER_PRODUCT = process.env.LITERS_PER_PRODUCT;
 
   try {
-  
-    const RESPONSE = {data: 'TESTING PHASE 7'};
-    /*
     // * GET ORDER
     const GET_ORDER_CONFIG = {
       method: 'get',
@@ -31,10 +20,11 @@ async function main(args) {
         'User-Agent': USER_AGENT
       }
     };
-    const ORDER = await axios(GET_ORDER_CONFIG);
-    
+    const ORDER_RESPONSE = await axios(GET_ORDER_CONFIG);
+    const ORDER = ORDER_RESPONSE?.data;
+
     // * GET DATA FROM ORDER
-    const ORDER_LITERS = ORDER?.products[0]?.quantity;
+    const ORDER_LITERS = ORDER?.products[0]?.quantity * LITERS_PER_PRODUCT;
     const ORDER_CUSTOMER = ORDER?.customer;
     
     // * GET USER
@@ -47,18 +37,20 @@ async function main(args) {
         'User-Agent': USER_AGENT
       }
     };
-    const CUSTOMER = await axios(GET_USER_CONFIG);
-    
+    const CUSTOMER_RESPONSE = await axios(GET_USER_CONFIG);
+    const CUSTOMER = CUSTOMER_RESPONSE?.data;
+
     // * GET PRODUCT BOUGHT
-    const CUSTOMER_ACTUAL_LITERS = CUSTOMER?.extra?.liters ? CUSTOMER.extra.liters : 0;
-    const CUSTOMER_UPDATED_LITERS = CUSTOMER_ACTUAL_LITERS + ORDER_LITERS;
+    const CUSTOMER_ACTUAL_LITERS = CUSTOMER?.note ? CUSTOMER.note : 0;
+    const CUSTOMER_UPDATED_LITERS = parseInt(CUSTOMER_ACTUAL_LITERS) + parseInt(ORDER_LITERS);
     
     // * UPDATE USER INFORMATION
+    // DISCOVER HOW TO MAKE CORRECTLY THIS REQUEST
     const UPDATE_USER_OBJECT = {
-      "extra": {
-        "liters": CUSTOMER_UPDATED_LITERS
-      }
+      "id": CUSTOMER?.id,
+      "note": CUSTOMER_UPDATED_LITERS.toString()
     }
+
     const UPDATE_USER_CONFIG = {
       method: 'put',
       url: `${API_URL}/customers/${ORDER_CUSTOMER?.id}`,
@@ -66,10 +58,14 @@ async function main(args) {
         'Content-Type': 'application/json',
         'Authentication': `bearer ${AUTH_TOKEN}`,
         'User-Agent': USER_AGENT
-      }
+      },
+      data: UPDATE_USER_OBJECT 
     };
-    const CUSTOMER_UPDATED = await axios(UPDATE_USER_CONFIG);
-    */
+    const CUSTOMER_UPDATED_RESPONSE = await axios(UPDATE_USER_CONFIG);
+    const CUSTOMER_UPDATED = CUSTOMER_UPDATED_RESPONSE?.data;
+
+    const RESPONSE = {data: `Customer ${CUSTOMER?.id} liters where updated from ${CUSTOMER_ACTUAL_LITERS} to ${CUSTOMER_UPDATED?.note}` };
+    console.log('> output  :', RESPONSE);
 
     return {
       statusCode: 200,
@@ -80,5 +76,6 @@ async function main(args) {
       statusCode: 500,
       body: JSON.stringify({ error: error.message })
     };
+ 
   }
 }
